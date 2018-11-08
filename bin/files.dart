e560302b525cc01c1fba89146f04a289734102e8
Denis Beurive <denis.beurive@gmail.com>
2018-11-08 17:11:24 +0100

    Add experimentation on files and more content about streams.

---
+++ bin/files.dart
@@ -0,0 +1,80 @@
+// The script illustrates the manipulations on files.
+// If you use programming languages such as PHP, Perl or Python, be aware that
+// many operations on files execute asynchronously.
+
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
+main() async {
+
+  File file = File('test.txt');
+
+  // Write 5 lines to the file.
+  //
+  // Please note that write operations are buffered. You can't be sure that the
+  // data has effectively been written to the file before the later is closed.
+  IOSink fs = file.openWrite();
+  for (int i=1; i<6; i++) {
+    fs.write('Line ' + i.toString() + "\n");
+  }
+
+  // Warning !
+  //
+  // The method "File.close()" executes asynchronously ! Thus, if you do not wait
+  // for the call to terminate, then you will continue the script's execution
+  // while all the data is not written to the file yet.
+  await fs.close();
+
+  // ---------------------------------------------------------------------------
+  // TEST 1: read a file using "listen".
+  // ---------------------------------------------------------------------------
+
+  // Please note that we create a stream. Right now, the stream is "just" created.
+  // It does not start generating events.
+  Stream<List<int>> file_stream =  file.openRead();
+
+  // Read the content of the file. We apply a listener to the stream. Then, it
+  // starts to generate events.
+  // Note: on my system, the maximum size for a chuck of data is 65536 bytes.
+  //       However, this size depends on the system the script is executed on.
+  file_stream.listen((List<int> data) {
+    String content = utf8.decode(data);
+    print("One chuck of data has been read. It contains a sequence of ${data.length} bytes. Decoded from UTF8:\n${content}");
+  }).onDone(() => print('Done with [listen()]'));
+
+  // ---------------------------------------------------------------------------
+  // TEST 2: read a file using "await for".
+  // ---------------------------------------------------------------------------
+
+  // Remember: a single subscription stream can not be used twice.
+  file_stream =  file.openRead();
+
+  await for (List<int> data in file_stream) {
+    String content = utf8.decode(data);
+    print("One chuck of data has been read. It contains a sequence of ${data.length} bytes. Decoded from UTF8:\n${content}");
+  }
+  print("Done with [await for]");
+
+  // ---------------------------------------------------------------------------
+  // TEST 3: read a file using the recommended ways.
+  // ---------------------------------------------------------------------------
+
+  String content = file.readAsStringSync();
+  print("[SYNC] The content of the file is:\n${content}");
+
+  await file.readAsString().then((content) {
+    print("[ASYNC] The content of the file is:\n${content}");
+  });
+
+  // The code below executes asynchronously.
+
+  file.openRead()
+      .transform(utf8.decoder)       // Decode bytes to UTF-8.
+      .transform(new LineSplitter()) // Convert stream to individual lines.
+      .listen((String line) {        // Process results.
+        print('$line: ${line.length} bytes');
+      },
+      onDone: () { print('File is now closed.'); },
+      onError: (e) { print(e.toString()); });
+}
--- bin/stream.dart
+++ bin/stream.dart
@@ -1,3 +1,5 @@
+
+
 // https://www.dartlang.org/tutorials/language/streams
 // https://www.dartlang.org/articles/libraries/creating-streams
 
@@ -7,13 +9,16 @@ import 'dart:math';
 
 class Container {
   static Random _random_generator = Random(123);
+  static int count_ten_and_above = 0;
 
   // This function creates a stream of integers.
-  static Stream<int> streamer() async* {
+  static Stream<int> streamer([int count=2, String prefix='']) async* {
+    count_ten_and_above = 0;
     for (int i=0; i<3; i++) {
-      int v = _random_generator.nextInt(1000);
-      print("Wait 2 seconds and then generate a value ${v}");
-      sleep(Duration(seconds: 2));
+      int v = _random_generator.nextInt(10) + _random_generator.nextInt(10);
+      print("[${prefix}] Wait 2 seconds and then generate a value ${v}");
+      sleep(Duration(seconds: count));
+      if (v > 10) { count_ten_and_above++; }
       yield v;
     }
   }
@@ -64,7 +69,7 @@ main() async {
   // asBroadcastStream to create a broadcast stream on top of the non-broadcast
   // stream.
   print("Create a broadcast stream. Note that it does not start to emit values until it gets assigned a listener.");
-  s = Container.streamer().asBroadcastStream();
+  s = Container.streamer(5).asBroadcastStream();
   print("Wait 5 seconds to show that the emission has not started yet.");
 
   // Note: now the stream is assigned a listener! So it starts emitting values.
@@ -76,6 +81,38 @@ main() async {
   // The stream is assigned another listener!
   // This listener listens **for the last value ONLY**.
   s.last.then((int value) => print("Last: $value"));
+
+  // ---------------------------------------------------------------------------
+  // TEST 3: create new streams based on existing ones.
+  // ---------------------------------------------------------------------------
+
+  // The new stream should have only one event, since:
+  // - The stream "s", from which the new stream is built upon, had 5 events.
+  // - 2 events have been consumed: the first one and the last one ("s.first..."
+  //   and "s.last...").
+  // - From the 3 remaining events, the first 2 ones are not transferred to the
+  //   new stream.
+  print("Create a new stream based on the existing one.");
+  Stream<int> duplicated = s.skip(2);
+  duplicated.length.then((int value) {
+    assert(1 == value);
+    print("The new stream has $value element(s).");
+  });
+
+  s = Container.streamer(5, 'A');
+  print("Create a new stream of integers which values are greater that 10. These stream should contain ${Container.count_ten_and_above} integers.");
+  duplicated = s.where((value) => value > 10);
+  duplicated.length.then((value) {
+    assert(value == Container.count_ten_and_above);
+    print("The new stream contains $value integers (expected ${Container.count_ten_and_above}).");
+  });
+
+  s = Container.streamer(5, 'A');
+  Stream<int> mapped = s.map((value) => 2*value);
+  await mapped.forEach((value) {
+    print("- mapped ${value}");
+  });
+
 }
 
 
