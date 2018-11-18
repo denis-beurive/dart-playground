f17d958b218bbe9b56d34a96970cb04e0d9bf7da
Denis BEURIVE <denis.beurive@gmail.com>
2018-11-18 20:33:52 +0100

    Add tests on dynamics

--- bin/types.dart
+++ bin/types.dart
@@ -28,4 +28,29 @@ main() {
   print(f1(10,20));
   CasterFunction f2 = MyCasterFunction();
   print(f2(10));
+
+  // ---------------------------------------------------------------------------
+  // TEST: dynamics
+  // ---------------------------------------------------------------------------
+
+  dynamic a = [1, 'a']; // a is a list.
+  print("Is a the variable \"a\" a list<dynamic> ? ${a is List<dynamic> ? 'yes' : 'no'}");
+
+  // Or...
+
+  List<dynamic> b = [1, 'a'];
+  print("Is a the variable \"b\" a list<dynamic> ? ${b is List<dynamic> ? 'yes' : 'no'}");
+
+  // Or...
+
+  var c = [1, 'a']; // a is a list.
+  print("Is a the variable \"c\" a list<dynamic> ? ${c is List<dynamic> ? 'yes' : 'no'}");
+
+  // Please note that the answers to the questions below are:
+  // - yes
+  // - yes
+
+  var d = [1, 2]; // a is a list.
+  print("Is a the variable \"d\" a list<dynamic> ? ${d is List<dynamic> ? 'yes' : 'no'}"); // => yes
+  print("Is a the variable \"d\" a list<int> ? ${d is List<int> ? 'yes' : 'no'}"); // => yes
 }
